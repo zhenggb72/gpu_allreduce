54c53d54fd06ecab994dc69ba5b02f18cd996326
Vadlamudi, Aishwarya <aishwarya.vadlamudi@intel.com>
2023-06-28 15:57:43 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ONEAPI_DEVICE_SELECTOR=level_zero:gpu
-mpirun -np 4 ./allreduce -c 16 -t fp16 : -np 3 ./allreduce -c 16 -t fp16
+mpirun -np 4 ./allreduce -c 16 -t float : -np 3 ./allreduce -c 16 -t float
